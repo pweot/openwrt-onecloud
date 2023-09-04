2a682c58155d53f371583268c485ef9ac2c4ced7
pweot <pweot@126.com>
2023-09-05 07:31:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,6 @@
 # Modify default IP
 #!/bin/bash
 #=================================================
-sed -i "s/192.168.0.1/192.168.0.102/" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/192.168.2.2/" package/base-files/files/bin/config_generate
 sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite --force-depends/' package/Makefile
 
