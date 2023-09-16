934c53a350eba9874876e114ba13504adc8641f7
pweot <pweot@126.com>
2023-09-16 22:35:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git small https://github.com/pweot/small-package' >>feeds.conf.default
 
